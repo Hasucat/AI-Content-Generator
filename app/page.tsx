374cb7a949dab443f744ec6ed3c1eea93c020cdc
Hasucat <hasnain.alif14@gmail.com>
2024-12-27 02:26:17 +0600

    Setup and Authentication

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Alif Hasnain!</h2>
+      <Button>Bokachoda</Button>
+    </div>
+  );
+}
